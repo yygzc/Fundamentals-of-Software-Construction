e76e891abdb021a9263fd2d00d8a2d8f4461cba0
yygzc <1053727940@qq.com>
2024-03-25 00:28:16 +0800

    assignment4

---
+++ assignment4/Clock/Clock/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 使用事件机制，模拟实现一个闹钟功能
+ * 闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件
+ * 在闹钟走时时或者响铃时，在控制台显示提示信息
+ */
+
+namespace Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            int time = 0;
+            clock.Tick += (sender, args) =>
+            {
+                Console.WriteLine($"Tick.  Time:{time++}s");
+            };
+
+            clock.Alarm += (sender, args) =>
+            {
+                Console.WriteLine($"Alarm! Time:{time++}s");
+            };
+
+            clock.Start(5); // 模拟计时
+        }
+    }
+    public class Clock
+    {
+        public event EventHandler Tick;
+        public event EventHandler Alarm;
+        public void Start(int seconds)
+        {
+            for (int i = 0; i < seconds; i++)
+            {
+                OnTick();
+                Thread.Sleep(1000); // 等待时长为1s
+            }
+            OnAlarm();
+        }
+        protected virtual void OnTick()
+        {
+            Tick?.Invoke(this, EventArgs.Empty); // 若事件不为null，则调用
+        }
+        protected virtual void OnAlarm()
+        {
+            Alarm?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ assignment4/ModifiedGenericList/ModifiedGenericList/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * 1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法,
+ * 通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）
+*/
+
+namespace ModifiedGenericList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            for(int x=0; x<10; x++)
+            {
+                list.Add(x);
+            }
+            //打印链表元素
+            GenericList<int>.ForEach<int>(list, m => Console.WriteLine(m));
+
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            GenericList<int>.ForEach<int>(list, 
+                m =>
+                {
+                    sum += m;
+                    if (m > max) max = m;
+                    if (m < min) min = m;
+                });
+            Console.WriteLine($"Sum={sum} Max={max} Min={min}");
+        }
+    }
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public static void ForEach<T>(GenericList<T> list, Action<T> action)
+        {
+            Node<T> p = list.head;
+            while(p != null)
+            {
+                action(p.Data);
+                p = p.Next;
+            }
+        }
+    }
+}
